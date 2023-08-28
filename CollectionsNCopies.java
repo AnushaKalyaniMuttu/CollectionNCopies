523dcaba99efe6c199bd7741d9d9472a835a2896
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2023-08-28 13:26:07 +0530

    Create CollectionsNCopies.java

---
+++ CollectionsNCopies.java
@@ -0,0 +1,18 @@
+package com.bean;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsNCopies {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Collections.nCopies(copiesLimit,copyValue);
+		 * s
+		 */
+	List<String> copies=	Collections.nCopies(5, "Anusha");
+	System.out.println(copies);
+	      	}
+		
+	}
